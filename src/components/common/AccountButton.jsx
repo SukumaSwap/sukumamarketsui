033b7bd77d6134d3d8fb952e3e51d94a0e1220d1
timothyAgevi <timothyagevi@gmail.com>
2022-11-09 23:29:09 +0300

    Connection to EVM networks

--- src/components/common/AccountButton.jsx
+++ src/components/common/AccountButton.jsx
@@ -1,118 +1,161 @@
-import { Menu, Button, Text, Group, CopyButton, ActionIcon } from '@mantine/core';
-import { IconCopyright, IconCopy, IconUser, IconLogout, IconLogin } from '@tabler/icons';
-import { getTextCount, getTheme } from '../../app/appFunctions';
-import { login, logout } from '../../app/near/utils';
-import { useSelector } from 'react-redux';
-import { selectNetwork } from '../../features/app/appSlice';
-import { showNotification } from '@mantine/notifications';
-import { useMantineTheme } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
-
+import {
+  Menu,
+  Button,
+  Text,
+  Group,
+  CopyButton,
+  ActionIcon,
+} from "@mantine/core";
+import {
+  IconCopyright,
+  IconCopy,
+  IconUser,
+  IconLogout,
+  IconLogin,
+} from "@tabler/icons";
+import { getTextCount, getTheme } from "../../app/appFunctions";
+import { login, logout } from "../../app/near/utils";
+import { useSelector } from "react-redux";
+import { selectNetwork } from "../../features/app/appSlice";
+import { showNotification } from "@mantine/notifications";
+import { useMantineTheme } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 function ConnectWalletButton() {
-    const network = useSelector(selectNetwork)
-    const dologin = () => {
-        if (network?.network_id === "near") {
-            login()
-            return;
-        }
-        else {
-            showNotification({
-                message: "Network has not been fully configured.",
-                color: "yellow"
-            })
-        }
+  const network = useSelector(selectNetwork);
+  const dologin = () => {
+    if (network?.network_id === "near") {
+      login();
+      return;
+    } else {
+      showNotification({
+        message: "Network has not been fully configured.",
+        color: "yellow",
+      });
     }
-    return (
-        <Button radius="xl" style={{
+  };
+
+  return (
+    <>
+      {network.network_id === "near" ? (
+        <Button
+          radius="xl"
+          style={{
             height: "42px",
             padding: "0 30px",
-        }} color="violet" onClick={dologin}>
-            Connect Wallet
+          }}
+          color="violet"
+          onClick={dologin}
+        >
+          Connect Wallet
         </Button>
-    )
+      ) : (
+        <>
+        <ConnectButton/>
+        </>
+      )}
+    </>
+  );
 }
 
 function SmConnectWalletButton() {
-    const theme = useMantineTheme()
-    const network = useSelector(selectNetwork)
-    const dologin = () => {
-        if (network?.network_id === "near") {
-            login()
-            return;
-        }
-        else {
-            showNotification({
-                message: "Network has not been fully configured.",
-                color: "yellow"
-            })
-        }
+  const theme = useMantineTheme();
+  const network = useSelector(selectNetwork);
+  const dologin = () => {
+    if (network?.network_id === "near") {
+      login();
+      return;
+    } else {
+      showNotification({
+        message: "Network has not been fully configured.",
+        color: "yellow",
+      });
     }
-    return (
-        <ActionIcon
-            variant='filled'
-            color={getTheme(theme) ? 'violet' : 'blue'}
-            onClick={dologin}
-            title="Open Network Drawer"
-            radius="md"
-        >
-            <IconLogin />
-        </ActionIcon>
-    )
+  };
+  return (
+    <ActionIcon
+      variant="filled"
+      color={getTheme(theme) ? "violet" : "blue"}
+      onClick={dologin}
+      title="Open Network Drawer"
+      radius="md"
+    >
+      <IconLogin />
+    </ActionIcon>
+  );
 }
 
 function AccountButton() {
-    let acc = window.walletConnection.getAccountId()
-    const navigate = useNavigate()
-    const goTo = (url) => {
-        navigate(url)
-    }
-    return (
-        <Menu shadow="md" width={230} radius="lg">
-            <Menu.Target>
-                <Button radius="xl" style={{
-                    height: "42px",
-                    padding: "0 30px",
-                }} color="violet">
-                    <Group>
-                        <Text >
-                            {getTextCount(acc || '', 13)}
-                        </Text>
-                        <CopyButton value={acc || ''}>
-                            {({ copied, copy }) => (
-                                <div color={copied ? 'teal' : 'blue'} onClick={copy}>
-                                    {copied ? <IconCopyright /> : <IconCopy />}
-                                </div>
-                            )}
-                        </CopyButton>
-                    </Group>
-                </Button>
-            </Menu.Target>
+  let acc = window.walletConnection.getAccountId();
+  const navigate = useNavigate();
+  const goTo = (url) => {
+    navigate(url);
+  };
+  return (
+    <Menu shadow="md" width={230} radius="lg">
+      <Menu.Target>
+        <Button
+          radius="xl"
+          style={{
+            height: "42px",
+            padding: "0 30px",
+          }}
+          color="violet"
+        >
+          <Group>
+            <Text>{getTextCount(acc || "", 13)}</Text>
+            <CopyButton value={acc || ""}>
+              {({ copied, copy }) => (
+                <div color={copied ? "teal" : "blue"} onClick={copy}>
+                  {copied ? <IconCopyright /> : <IconCopy />}
+                </div>
+              )}
+            </CopyButton>
+          </Group>
+        </Button>
+      </Menu.Target>
 
-            <Menu.Dropdown px="md" py="xl">
-                <Menu.Item sx={theme => ({
-                    background: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1],
-                    borderRadius: theme.radius.md,
-                    height: 50
-                })} px="xl" mb="xs" onClick={e => goTo("/market")}>
-                    <Group position='center'>
-                        <IconUser />
-                        <Text>Profile</Text>
-                    </Group>
-                </Menu.Item>
-                <Menu.Item sx={theme => ({
-                    background: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1],
-                    borderRadius: theme.radius.md,
-                    height: 50
-                })} px="xl" mb="xs" onClick={logout}>
-                    <Group position='center'>
-                        <Text>Logout</Text>
-                        <IconLogout />
-                    </Group>
-                </Menu.Item>
-            </Menu.Dropdown>
-        </Menu>
-    );
+      <Menu.Dropdown px="md" py="xl">
+        <Menu.Item
+          sx={(theme) => ({
+            background:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[4]
+                : theme.colors.gray[1],
+            borderRadius: theme.radius.md,
+            height: 50,
+          })}
+          px="xl"
+          mb="xs"
+          onClick={(e) => goTo("/market")}
+        >
+          <Group position="center">
+            <IconUser />
+            <Text>Profile</Text>
+          </Group>
+        </Menu.Item>
+        <Menu.Item
+          sx={(theme) => ({
+            background:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[4]
+                : theme.colors.gray[1],
+            borderRadius: theme.radius.md,
+            height: 50,
+          })}
+          px="xl"
+          mb="xs"
+          onClick={logout}
+        >
+          <Group position="center">
+            <Text>Logout</Text>
+            <IconLogout />
+          </Group>
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
 }
 
 export default AccountButton;
-export { ConnectWalletButton, SmConnectWalletButton }
+export { ConnectWalletButton, SmConnectWalletButton };
--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,36 +1,67 @@
-import { useState, useEffect } from 'react';
-import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
-import { BrowserRouter } from 'react-router-dom';
-import { NotificationsProvider } from '@mantine/notifications';
-import { ModalsProvider } from '@mantine/modals';
-import { useSelector } from 'react-redux';
-import { selectTheme } from '../../features/app/appSlice';
+import { useState, useEffect } from "react";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+import { NotificationsProvider } from "@mantine/notifications";
+import { ModalsProvider } from "@mantine/modals";
+import { useSelector } from "react-redux";
+import { selectTheme } from "../../features/app/appSlice";
+
+//wagmi imports
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+//configs
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+//connectors
+const { connectors } = getDefaultWallets({
+  appName: " Thee Landcoin",
+  chains,
+});
+// client : connector + provider
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export function AppWrapper({ children }) {
-
-  const [colorScheme, setColorScheme] = useState('dark');
-  const theme = useSelector(selectTheme)
+  const [colorScheme, setColorScheme] = useState("dark");
+  const theme = useSelector(selectTheme);
 
   const toggleColorScheme = (value) => {
-    setColorScheme(value)
-  }
-
+    setColorScheme(value);
+  };
 
   useEffect(() => {
     setColorScheme(theme);
-  }, [theme])
+  }, [theme]);
 
   return (
     <BrowserRouter>
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={{ colorScheme, fontFamily: 'Mukta' }} withGlobalStyles withNormalizeCSS>
-          <ModalsProvider>
-            <NotificationsProvider>
-              {children}
-            </NotificationsProvider>
-          </ModalsProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={toggleColorScheme}
+          >
+            <MantineProvider
+              theme={{ colorScheme, fontFamily: "Mukta" }}
+              withGlobalStyles
+              withNormalizeCSS
+            >
+              <ModalsProvider>
+                <NotificationsProvider>{children}</NotificationsProvider>
+              </ModalsProvider>
+            </MantineProvider>
+          </ColorSchemeProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </BrowserRouter>
   );
 }
